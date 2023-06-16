efb6bb82c5c72b7cad476c6440095daecd7a6a45
Paulo-vitorCS <paulo.vitorcs@hotmail.com>
2023-06-16 17:21:52 -0300

    Created message queue using RabbitMQ

--- src/main/java/br/com/phoneRecharges/app/Application.java
+++ src/main/java/br/com/phoneRecharges/app/Application.java
@@ -1,11 +1,13 @@
 package br.com.phoneRecharges.app;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableRabbit
 @SpringBootApplication
 @ComponentScan("br.com.phoneRecharges")
 @EntityScan("br.com.phoneRecharges.domain")
