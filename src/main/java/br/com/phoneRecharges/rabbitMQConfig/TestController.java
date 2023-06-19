44a24c069ae4b0a66a2c606d3984bb5a871f0b0d
Paulo-vitorCS <paulo.vitorcs@hotmail.com>
2023-06-19 14:05:58 -0300

    Exchange set up

--- src/main/java/br/com/phoneRecharges/rabbitMQConfig/TestController.java
+++ src/main/java/br/com/phoneRecharges/rabbitMQConfig/TestController.java
@@ -1,5 +1,8 @@
 package br.com.phoneRecharges.rabbitMQConfig;
 
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,13 +12,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 public class TestController {
 
-    @Autowired
-    private QueueSender queueSender;
+    private final AmqpTemplate queueSender;
+
+    public TestController(AmqpTemplate queueSender) {
+        this.queueSender = queueSender;
+    }
 
     @GetMapping
     public String send() {
-        queueSender.send("Test message");
-        return "Ok. Done";
+        String msg = "test message";
+
+        MessageProperties messageProperties = new MessageProperties();
+        messageProperties.setHeader("last", "yes");
+
+        Message message = new Message(msg.getBytes(), messageProperties);
+
+
+        queueSender.convertAndSend("test", "routing-key-test", message);
+        return "Ok. Done!";
     }
 
 }
